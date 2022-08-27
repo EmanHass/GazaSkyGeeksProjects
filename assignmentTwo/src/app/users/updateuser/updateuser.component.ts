7823ffbe4d87e8d5d2b8cd97ea557085cd14bc94
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2022-08-27 20:42:53 +0300

    Update users methods

--- assignmentTwo/src/app/users/updateuser/updateuser.component.ts
+++ assignmentTwo/src/app/users/updateuser/updateuser.component.ts
@@ -9,45 +9,45 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./updateuser.component.scss']
 })
 export class UpdateuserComponent implements OnInit {
-    id:string;
-    registrationFG: FormGroup;
-    success:boolean=false;
+  id: string;
+  registrationFG: FormGroup;
+  success: boolean = false;
 
-  constructor(private route: ActivatedRoute, private usersService:UsersService) {
+  constructor(private route: ActivatedRoute,private usersService: UsersService) {
     this.initializationFG();
-   }
+  }
+  ngOnInit(): void {
+    if (this.route.snapshot.paramMap.has('id')) {
+      this.id = this.route.snapshot.paramMap.get('id');
+    }
 
-   initializationFG(): void {  
-    this.registrationFG= new FormGroup({
-      id: new FormControl('',Validators.required),
-      name: new FormGroup({
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+    });
+    this.registrationFG.setValue(this.usersService.getUserById(this.id));
+  }
+  initializationFG(): void {
+    this.registrationFG = new FormGroup({
+      id: new FormControl('', Validators.required),
+      name: this.initializationName(),
+      birthDate: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+    });
+  }
+  initializationName(): FormGroup {
+      return new FormGroup({
         firstName: new FormControl('',Validators.required),
         lastName: new FormControl('',Validators.required)
-      }),
-      birthDate: new FormControl('',Validators.required),
-      email: new FormControl('',[Validators.required,Validators.email]),
-      phone:new FormControl('',Validators.required),
-      address: new FormControl('',Validators.required)
-    })
+      })
   }
 
-  ngOnInit(): void {
-    if(this.route.snapshot.paramMap.has('id')){
-       this.id= this.route.snapshot.paramMap.get('id');
-    }
-    
-    this.route.params.subscribe(
-      (params:Params)=>{
-        this.id= params['id'];
-      }
-    )
-    this.registrationFG.setValue(this.usersService.getUserById(this.id))
-  }
-
-  onUpdate():void{
-    if(this.registrationFG.valid){
-    this.usersService.updateUser(this.id,this.registrationFG.value);
-    this.success=true;
+  // when click on update button
+  onUpdate(): void {
+    if (this.registrationFG.valid) {
+      this.usersService.updateUser(this.id, this.registrationFG.value);
+      this.success = true;
     }
   }
 }

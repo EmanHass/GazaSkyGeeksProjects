30453c52503ce28d06f1aba4f6664ab6734cdb75
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2022-09-16 23:02:05 +0300

    add implements for delete selected user

--- assignmentThree/src/app/users/components/list/list.component.ts
+++ assignmentThree/src/app/users/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { concatMap, from } from 'rxjs';
 import { Users } from '../../model-interface/model';
 import { UserService } from '../../users-service/user.service';
 
@@ -9,13 +10,16 @@ import { UserService } from '../../users-service/user.service';
 })
 export class ListComponent implements OnInit {
   users: Users[];
+  id: number;
   isLoading: boolean= false;
-  userDetails: Users;
+  isDelete: boolean= false;
+  msgDelete: string;
   page: number=1;
   perPage: number=3;
   totalPage: number;
   totalUserArr: number[]=[];
   totalPageArr: number[]=[];
+  userChecked: boolean[];
   constructor(private usersService: UserService) { }
 
   ngOnInit(): void {
@@ -30,6 +34,8 @@ export class ListComponent implements OnInit {
         this.totalPage = res.total_pages;
         this.totalUserArr = Array.from(new Array(res.total).keys(), (item)=>item+1)
         this.totalPageArr = Array.from(new Array(this.totalPage).keys(), (item)=> item+1)
+        this.userChecked = Array.from(new Array(res.data.length).keys(), (item)=> false)
+
         this.isLoading=false
       },
       error=>{
@@ -39,21 +45,25 @@ export class ListComponent implements OnInit {
   }
   deleteUser(id: number):void{
       this.usersService.delete(id).subscribe(
-        res=>console.log('delted done'),
+        res=>{
+          this.id=id;
+          this.isDelete=true;
+          this.msgDelete=`User #${this.id} Deleted`;
+          setTimeout(() => this.isDelete=false, 2000);          
+        },
         error=> console.error(error)
-        
-        
       )
   }
 
   detailsUser(id:number):void{
     this.usersService.getByID(id).subscribe(
       (res: any)=> {
-        this.userDetails= res.data;
-        this.usersService.sendData(this.userDetails)      
+        this.usersService.sendData(res.data)      
+      },
+      error => {
+        console.log(error)
       } 
-    )
-    
+    )  
   }
   getPrivous(): void{
     this.getUsers(this.page - 1, this.perPage)
@@ -67,6 +77,37 @@ export class ListComponent implements OnInit {
   getValue(event: any): void{
     this.perPage=event.target.value;
     this.getUsers(1, this.perPage)
+  }
+
+  toggleCheck(event: any, index:number): void{
+    this.userChecked[index]=event.target.checked;
+  }
+  deleteSelectedUsers(): void{
+    var checkedId: number[]=[];
+    this.userChecked.forEach((value,index)=>{
+      if(value){
+        checkedId.push(this.users[index].id);
+      }
+    })
+
+    var obs = from(checkedId);    
+    obs.pipe(
+      concatMap((value)=>{
+        this.id=value;
+        this.isDelete=true;
+        this.msgDelete=`User #${value} Deleted`;
+        setTimeout(() => this.isDelete=false, 5000);
+        return this.usersService.delete(value)
+      })
+    ).subscribe(
+      res=>{
+        console.log(`user Deleted`);
+      },
+      error=>{
+        console.log('error', error);
+        
+      }
+    )
   }
 
 }

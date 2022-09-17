3cdeb47190df25160da6f3f11ad5b61d782ae294
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2022-09-17 23:22:48 +0300

    Update some files

--- assignmentThree/src/app/users/users-service/user.service.ts
+++ assignmentThree/src/app/users/users-service/user.service.ts
@@ -1,58 +1,62 @@
 import { Users } from './../model-interface/model';
-import { HttpClient, HttpClientModule, HttpHeaders, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class UserService{
+export class UserService {
   apiLink: string = 'https://reqres.in/api/users';
-  subjectData: Subject<any>= new Subject<any>();
+  subjectData: Subject<any> = new Subject<any>();
   DataObservable: Observable<any> = this.subjectData.asObservable();
-  
-  constructor(private http: HttpClient) { }
 
-  getAll(page: number, perPage: number): Observable<Users[]>{
+  constructor(private http: HttpClient) {}
+
+  getAll(page: number, perPage: number): Observable<Users[]> {
     let params = new HttpParams();
-    params= params.append('page',page);
-    params= params.append('per_page',perPage);
-    return this.http.get<Users[]>(`${this.apiLink}`,
-      {headers: new HttpHeaders({'content-type': 'application/json'}),
-       observe:'body',
-       params:params
-      })
+    params = params.append('page', page);
+    params = params.append('per_page', perPage);
+    return this.http.get<Users[]>(`${this.apiLink}`, {
+      headers: new HttpHeaders({ 'content-type': 'application/json' }),
+      observe: 'body',
+      params: params,
+    });
   }
 
-  getByID(id:number): Observable<Users>{
-      return this.http.get<Users>(`${this.apiLink}/${id}`);
+  getByID(id: number): Observable<Users> {
+    return this.http.get<Users>(`${this.apiLink}/${id}`);
   }
 
-  add(newUser: Users): Observable<Users>{
-      return this.http.post<Users>(this.apiLink, newUser,{
-        headers: new HttpHeaders({
-          'content-type':'application/json'
-        })
-      })
+  add(newUser: Users): Observable<Users> {
+    return this.http.post<Users>(this.apiLink, newUser, {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+      }),
+    });
   }
 
-  update(id: number, user:Users):Observable<any>{
-      return this.http.put(`${this.apiLink}/${id}`,user, {
-        headers: new HttpHeaders({
-          'content-type':'application/json'
-        })
-      })      
+  update(id: number, user: Users): Observable<any> {
+    return this.http.put(`${this.apiLink}/${id}`, user, {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+      }),
+    });
   }
 
-  delete(id:number): Observable<any>{
-   return  this.http.delete(`${this.apiLink}/${id}`);
+  delete(id: number): Observable<any> {
+    return this.http.delete(`${this.apiLink}/${id}`);
   }
 
-  sendData(value: any): void{
-    this.subjectData.next(value)
+  sendData(value: any): void {
+    this.subjectData.next(value);
   }
 
-  getData(): Observable<any>{
+  getData(): Observable<any> {
     return this.DataObservable;
   }
 }
